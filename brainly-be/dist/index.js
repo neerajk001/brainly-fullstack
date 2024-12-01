5ca80e0e089187b0b24f596e1d1d6b0cd9c8c869
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-12-02 00:44:48 +0530

    some changes in node modules

---
+++ brainly-be/dist/index.js
@@ -0,0 +1,63 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const db_1 = require("./db");
+const jwt_password = "bhbhdb3456";
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post('/api/v1/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    yield db_1.UserModel.create({
+        username: username,
+        password: password,
+    });
+    res.json({
+        message: 'you  signed up'
+    });
+}));
+app.post('/api/v1/signin', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = yield db_1.UserModel.findOne({
+        username,
+        password
+    });
+    if (existingUser) {
+        const token = jsonwebtoken_1.default.sign({
+            id: existingUser._id
+        }, jwt_password);
+        res.json({
+            token: token,
+        });
+    }
+    else {
+        res.status(403).json({
+            message: "incorrect credential"
+        });
+    }
+}));
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield mongoose_1.default.connect("mongodb+srv://neeraj001:heyramheyram001@cluster0.rlawr.mongodb.net/third-brain");
+        console.log("the database has connected");
+        app.listen(3000, () => {
+            console.log("listening at port 3000");
+        });
+    });
+}
+main();
