38b4356ba46185a8980f6efdf4752c19bf9ac394
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-12-02 00:39:53 +0530

    added signup point with jwt authentication

--- brainly-be/src/index.ts
+++ brainly-be/src/index.ts
@@ -2,7 +2,7 @@ import  Express  from "express";
 import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
 import { UserModel } from "./db";
-
+const jwt_password ="bhbhdb3456"
 const app = Express();
 app.use(Express.json());
 
@@ -20,6 +20,36 @@ app.post('/api/v1/signup' ,async  (req ,res)  =>{
         message:'you  signed up'
     })
 })
+
+app.post('/api/v1/signin',async(req ,res)=>{
+    const username =req.body.username;
+    const password =req.body.password;
+
+    const existingUser =await UserModel.findOne({
+        username,
+        password
+
+    })
+
+    if(existingUser){
+        const token = jwt.sign({
+            id: existingUser._id
+        }, jwt_password)
+
+        res.json({
+            token:token,
+        })
+
+    }else{
+        res.status(403).json({
+            message:"incorrect credential"
+        })
+    }
+
+    
+})
+
+
 
 async function main(){
     await mongoose.connect("mongodb+srv://neeraj001:heyramheyram001@cluster0.rlawr.mongodb.net/third-brain")
