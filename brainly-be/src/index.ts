3fe92f11c4d48bcf4273b390b62788a164c0be97
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-12-02 22:29:28 +0530

    added content routes with get and dlt and post

--- brainly-be/src/index.ts
+++ brainly-be/src/index.ts
@@ -1,10 +1,11 @@
 import  Express  from "express";
 import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
-import { UserModel } from "./db";
+import { UserModel, contentModel } from './db';
 import { jwt_password } from "./config";
 const app = Express();
 app.use(Express.json());
+import { userMiddleware } from "./middleWare";
 
 
 app.post('/api/v1/signup' ,async  (req ,res)  =>{
@@ -47,6 +48,50 @@ app.post('/api/v1/signin',async(req ,res)=>{
     }
 
     
+})
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+    await contentModel.create({
+        link,
+        type,
+        title: req.body.title,
+        //@ts-ignore
+        userId: req.userId,
+        tags: []
+    })
+
+    res.json({
+        message: "Content added"
+    })
+    
+})
+
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    //@ts-ignore
+    const userId =req.userId;
+    const content = await contentModel.find({
+        userId:userId 
+    }).populate("userId" ,"username")
+    res.json({
+        content
+    })
+}
+
+)
+
+app.delete("/api/v1/content",userMiddleware,async(req ,res) =>{
+    const contentId =req.body.contentId;
+    //@ts-ignore
+    const userId =req.userId;
+    await contentModel.deleteMany({
+        contentId,
+        userId
+    })
+    res.json({
+        message:"deleted"
+    })
 })
 
 
