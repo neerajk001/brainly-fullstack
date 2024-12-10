a3febb7d86ecf310c8be81fbcc59c6e50181ced8
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-12-10 20:35:49 +0530

    brain routes added

--- brainly-be/src/index.ts
+++ brainly-be/src/index.ts
@@ -1,11 +1,12 @@
 import  Express  from "express";
 import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
-import { UserModel, contentModel } from './db';
+import { UserModel, contentModel, linkModel } from './db';
 import { jwt_password } from "./config";
 const app = Express();
 app.use(Express.json());
 import { userMiddleware } from "./middleWare";
+import  { random } from './utils'
 
 
 app.post('/api/v1/signup' ,async  (req ,res)  =>{
@@ -93,6 +94,80 @@ app.delete("/api/v1/content",userMiddleware,async(req ,res) =>{
         message:"deleted"
     })
 })
+
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share;
+    if (share) {
+            const existingLink = await linkModel.findOne({
+                //@ts-ignore
+                userId: req.userId
+            });
+
+            if (existingLink) {
+                res.json({
+                    hash: existingLink.hash
+                })
+                return;
+            }
+            const hash = random(10);
+            await linkModel.create({
+                //@ts-ignore
+                userId: req.userId,
+                hash: hash
+            })
+
+            res.json({
+                hash
+            })
+    } else {
+        await linkModel.deleteOne({
+            //@ts-ignore
+            userId: req.userId
+        });
+
+        res.json({
+            message: "Removed link"
+        })
+    }
+})
+
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink;
+
+    const link = await linkModel.findOne({
+        hash
+    });
+
+    if (!link) {
+        res.status(411).json({
+            message: "Sorry incorrect input"
+        })
+        return;
+    }
+    // userId
+    const content = await contentModel.find({
+        userId: link.userId
+    })
+
+    
+    const user = await UserModel.findOne({
+        _id: link.userId
+    })
+
+    if (!user) {
+        res.status(411).json({
+            message: "user not found, error should ideally not happen"
+        })
+        return;
+    }
+
+    res.json({
+        username: user.username,
+        content: content
+    })
+
+})
+
 
 
 
