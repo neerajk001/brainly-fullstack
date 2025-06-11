c5e7b76c2f330a7ad061897fed1f79e1c371f2fa
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-06-11 16:55:12 +0530

    type fixes in middleware

--- brainly-be/src/index.ts
+++ brainly-be/src/index.ts
@@ -1,188 +1,110 @@
-import  Express  from "express";
-import mongoose from 'mongoose'
-import jwt from 'jsonwebtoken'
-import { UserModel, contentModel, linkModel } from './db';
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import cors from "cors";
+
+import { UserModel, contentModel, linkModel } from "./db";
 import { jwt_password } from "./config";
-import { userMiddleware } from "./middleWare";
-import  { random } from './utils'
-import cors from 'cors';
+import { userMiddleware, AuthenticatedRequest } from "./middleWare";
+import { random } from "./utils";
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const app = Express();
-app.use(Express.json());
-app.use(cors())
+app.post("/api/v1/signup", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
 
+  await UserModel.create({ username, password });
 
+  res.json({ message: "you signed up" });
+});
 
-app.post('/api/v1/signup' ,async  (req ,res)  =>{
-    const username =req.body.username;
-    const password =req.body.password;
+app.post("/api/v1/signin", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
 
-    await  UserModel.create({
-        username:username,
-        password:password,
-    })
-      
+  const existingUser = await UserModel.findOne({ username, password });
 
-    res.json({
-        message:'you  signed up'
-    })
-})
+  if (existingUser) {
+    const token = jwt.sign({ id: existingUser._id }, jwt_password);
+    res.json({ token });
+  } else {
+    res.status(403).json({ message: "incorrect credentials" });
+  }
+});
 
-app.post('/api/v1/signin',async(req ,res)=>{
-    const username =req.body.username;
-    const password =req.body.password;
+app.post("/api/v1/content", userMiddleware, async (req: AuthenticatedRequest, res: Response) => {
+  const { link, type, title } = req.body;
 
-    const existingUser =await UserModel.findOne({
-        username,
-        password
+  await contentModel.create({
+    link,
+    type,
+    title,
+    userId: req.userId,
+    tags: []
+  });
 
-    })
+  res.json({ message: "Content added" });
+});
 
-    if(existingUser){
-        const token = jwt.sign({
-            id: existingUser._id
-        }, jwt_password)
+app.get("/api/v1/content", userMiddleware, async (req: AuthenticatedRequest, res: Response) => {
+  const content = await contentModel.find({ userId: req.userId }).populate("userId", "username");
+  res.json({ content });
+});
 
-        res.json({
-            token:token,
-        })
+app.delete("/api/v1/content/:id", userMiddleware, async (req: AuthenticatedRequest, res: Response) => {
+  const contentId = req.params.id;
+  await contentModel.findOneAndDelete({ _id: contentId, userId: req.userId });
+  res.json({ message: "deleted" });
+});
 
-    }else{
-        res.status(403).json({
-            message:"incorrect credential"
-        })
+app.post("/api/v1/brain/share", userMiddleware, async (req: AuthenticatedRequest, res: Response) => {
+  const { share } = req.body;
+
+  if (share) {
+    const existingLink = await linkModel.findOne({ userId: req.userId });
+
+    if (existingLink) {
+      res.json({ hash: existingLink.hash });
+      return;
     }
 
-    
-})
+    const hash = random(10);
+    await linkModel.create({ userId: req.userId, hash });
+    res.json({ hash });
+  } else {
+    await linkModel.deleteOne({ userId: req.userId });
+    res.json({ message: "Removed link" });
+  }
+});
 
-app.post("/api/v1/content", userMiddleware, async (req, res) => {
-    const link = req.body.link;
-    const type = req.body.type;
-    await contentModel.create({
-        link,
-        type,
-        title: req.body.title,
-        //@ts-ignore
-        userId: req.userId,
-        tags: []
-    })
+app.get("/api/v1/brain/:shareLink", async (req: Request, res: Response) => {
+  const hash = req.params.shareLink;
+  const link = await linkModel.findOne({ hash });
 
-    res.json({
-        message: "Content added"
-    })
-    
-})
+  if (!link) {
+    res.status(411).json({ message: "Sorry incorrect input" });
+    return;
+  }
 
-app.get("/api/v1/content", userMiddleware, async (req, res) => {
-    //@ts-ignore
-    const userId =req.userId;
-    const content = await contentModel.find({
-        userId:userId 
-    }).populate("userId" ,"username")
-    res.json({
-        content
-    })
+  const content = await contentModel.find({ userId: link.userId });
+  const user = await UserModel.findById(link.userId);
+
+  if (!user) {
+    res.status(411).json({ message: "user not found, error should ideally not happen" });
+    return;
+  }
+
+  res.json({ username: user.username, content });
+});
+
+async function main() {
+  await mongoose.connect("mongodb+srv://neeraj001:heyramheyram001@cluster0.rlawr.mongodb.net/third-brain");
+  console.log("Database connected");
+
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
 }
 
-)
-
-app.delete("/api/v1/content/:id",userMiddleware,async(req ,res) =>{
-    const contentId =req.params.id;
-    //@ts-ignore
-    const userId =req.userId;
-    await contentModel.findOneAndDelete({
-       _id: contentId,
-        userId
-    })
-    res.json({
-        message:"deleted"
-    })
-})
-
-app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
-    const share = req.body.share;
-    if (share) {
-            const existingLink = await linkModel.findOne({
-                //@ts-ignore
-                userId: req.userId
-            });
-
-            if (existingLink) {
-                res.json({
-                    hash: existingLink.hash
-                })
-                return;
-            }
-            const hash = random(10);
-            await linkModel.create({
-                //@ts-ignore
-                userId: req.userId,
-                hash: hash
-            })
-
-            res.json({
-                hash
-            })
-    } else {
-        await linkModel.deleteOne({
-            //@ts-ignore
-            userId: req.userId
-        });
-
-        res.json({
-            message: "Removed link"
-        })
-    }
-})
-
-app.get("/api/v1/brain/:shareLink", async (req, res) => {
-    const hash = req.params.shareLink;
-
-    const link = await linkModel.findOne({
-        hash
-    });
-
-    if (!link) {
-        res.status(411).json({
-            message: "Sorry incorrect input"
-        })
-        return;
-    }
-    // userId
-    const content = await contentModel.find({
-        userId: link.userId
-    })
-
-    
-    const user = await UserModel.findOne({
-        _id: link.userId
-    })
-
-    if (!user) {
-        res.status(411).json({
-            message: "user not found, error should ideally not happen"
-        })
-        return;
-    }
-
-    res.json({
-        username: user.username,
-        content: content
-    })
-
-})
-
-
-
-
-async function main(){
-    await mongoose.connect("mongodb+srv://neeraj001:heyramheyram001@cluster0.rlawr.mongodb.net/third-brain")
-    console.log("the database has connected")
-    app.listen(3000 ,()=>{
-        console.log("listening at port 3000")
-       })
-}
-main()
-
+main();
--- brainly-be/src/middleWare.ts
+++ brainly-be/src/middleWare.ts
@@ -1,20 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-
 import { jwt_password } from './config';
 
-export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const header = req.headers["authorization"]
-    const decoded = jwt.verify(header as string ,jwt_password)
-
-    if(decoded){
-        //@ts-ignore
-        req.userId =decoded.id;
-        next()
-    }else{
-        res.status(403).json({
-            messgage:'you are not logged in'
-        })
-    }
-
+// Extend Request to include userId
+export interface AuthenticatedRequest extends Request {
+  userId?: string;
 }
+
+// ✅ Add return type `void` to make TS happy
+export const userMiddleware = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    res.status(401).json({ message: "Authorization header missing" });
+    return;
+  }
+
+  try {
+    const token = authHeader.split(" ")[1]; // Bearer <token>
+    const decoded = jwt.verify(token, jwt_password) as { id: string };
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
